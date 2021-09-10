fef67d881087e83d0f6636a1154446b5356b799c
TeroSimonen <terosimon@gmail.com>
2021-09-10 16:01:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const fahrenheitSyote = document.querySelector('input');
+const fahrenheitTuloste = document.querySelector('#fahrenheit');
+const celsiusTuloste = document.querySelector('#celsius');
+const muunnaButton = document.querySelector('button');
+
+function fahrenheitCelsiukseksi(fahrenheit){
+    let celsius = fahrenheit;
+    return celsius;
+
+}
+
+function muunna() {
+    let syote = fahrenheitSyote.value;
+    fahrenheitTuloste.textContent = syote;
+
+    
+
+    celsiusTuloste.textContent = fahrenheitCelsiukseksi(syote);
+}
+
+
+
+muunnaButton.addEventListener('click', muunna);
+
+
+
